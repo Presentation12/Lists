3340ea4b0229129eb6be8fc3fa630f035044d026
Presentation12 <joaocarlosapresentacao@gmail.com>
2022-05-11 18:46:27 +0100

    struct Job e intermedia

--- code/structs.h
+++ code/structs.h
@@ -4,37 +4,58 @@
  * @brief Estruturas do programa
  * @version 0.1
  * @date 2022-03-26
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 /**
  * @brief Estrutura que representa a lista das máquinas
- * 
+ *
  */
-typedef struct _listMachine
+typedef struct list_machine
 {
     int time, id_mac;
-    struct _listMachine *next, *previous;
+    struct list_machine *next, *previous;
 } Machine;
 
 /**
  * @brief Estrutura intermedia entre a lista das maquinas e das operações
- * 
+ *
  */
-typedef struct _listMacOp
+typedef struct list_macop
 {
     int id_op, id_mac;
-    struct _listMacOp *next, *previous;
+    struct list_macop *next, *previous;
 } MacOp;
 
 /**
  * @brief Estrutura que representa a lista das operações
- * 
+ *
  */
-typedef struct _listOperation
+typedef struct list_operation
 {
     int id_op;
-    struct _listOperation *next, *previous;
+    struct list_operation *next, *previous;
 } Operation;
+
+/**
+ * @brief Estrutura intermedia entre a lista das operações e dos jobs
+ * 
+ */
+typedef struct list_opjob
+{
+    int id_op;
+    int id_job;
+    struct list_opjob *next, *previous;
+} OpJob;
+
+/**
+ * @brief Estrutura que representa a lista dos jobs
+ * 
+ */
+typedef struct list_job
+{
+    int id_job;
+    struct list_job *next, *previous;
+} Job;
